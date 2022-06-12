85ca0194f12f36b7a04a8c4ab44fe5261c44ca03
Acauã Pitta <acaua.pitta@hotmail.com>
2022-06-11 22:43:48 -0300

    Atualiza condicionando impressão e previnindo deadlocks

--- formH2O.c
+++ formH2O.c
@@ -4,42 +4,50 @@
 #include <semaphore.h>
 #include <unistd.h>
 
+#define TRUE 1
+#define FALSE 0
+#define NUM_MOLECULAS 999
+
 void* thread_O(void* arg);
 void* thread_H(void* arg);
 void inicia_variaveis();
 void destroi_variaveis();
 void inicia_threads();
 void forma_h2o();
+void print_stock_and_finish_program();
 
 pthread_mutex_t mutex;
+pthread_mutex_t print_mutex;
 sem_t O_sem;
 sem_t H_sem;
 pthread_barrier_t barreira;
 int O = 0;
 int H = 0;
 int moleculas_lidas = 0;
+int pode_imprimir = FALSE;
 
 void inicia_variaveis() {
     pthread_barrier_init(&barreira, NULL, 3);
     sem_init(&O_sem, 0, 0);
     sem_init(&H_sem, 0, 0);
     pthread_mutex_init(&mutex, NULL);
+    pthread_mutex_init(&print_mutex, NULL);
 }
 
 void destroi_variaveis() {
     sem_destroy(&H_sem);
     sem_destroy(&O_sem);
     pthread_mutex_destroy(&mutex);
+    pthread_mutex_destroy(&print_mutex);
     pthread_barrier_destroy(&barreira);
 }
 
 void inicia_threads() {
     srand((unsigned)time(NULL));
-    int num_of_molecules = 99;
-    pthread_t molecules[num_of_molecules];
-    int threads_ids[num_of_molecules];
+    pthread_t molecules[NUM_MOLECULAS];
+    int threads_ids[NUM_MOLECULAS];
 
-    for (int i = 0; i <= num_of_molecules; i++) {
+    for (int i = 0; i < NUM_MOLECULAS; i++) {
         threads_ids[i] = i;
         int isH = rand() % 3;
         if (isH > 0) {
@@ -50,7 +58,7 @@ void inicia_threads() {
         }
     }
 
-    for (int i = 1; i <= num_of_molecules; i++) {
+    for (int i = 1; i <= NUM_MOLECULAS; i++) {
         pthread_join(molecules[i], NULL);
     }
 }
@@ -59,7 +67,8 @@ void* thread_O(void* arg) {
     pthread_mutex_lock(&mutex);
     moleculas_lidas++;
     O++;
-    printf("O recebido! quantidade total = %d - Thread: %d\n", O, *((int*)arg));
+    printf("O recebido! Quantidade O = %d - Total lidas: %d (Thread: %d)\n", O, moleculas_lidas, *((int*)arg));
+    fflush(stdout);
     if (H >= 2) {
         sem_post(&H_sem);
         sem_post(&H_sem);
@@ -68,14 +77,16 @@ void* thread_O(void* arg) {
         O--;
     }
     else {
-        pthread_mutex_unlock(&mutex);
+        if (moleculas_lidas == NUM_MOLECULAS && H < 2) {
+            print_stock_and_finish_program();
+        } else {
+            pthread_mutex_unlock(&mutex);
+        }
     }
 
     sem_wait(&O_sem);
-
     forma_h2o();
 
-    fflush(stdout);
     pthread_barrier_wait(&barreira);
     pthread_mutex_unlock(&mutex);
     return 0;
@@ -85,7 +96,8 @@ void* thread_H(void* arg) {
     pthread_mutex_lock(&mutex);
     moleculas_lidas++;
     H++;
-    printf("H recebido! quantidade total = %d - Thread: %d\n", H, *((int*)arg));
+    printf("H recebido! Quantidade H = %d - Total lidas: %d (Thread: %d)\n", H, moleculas_lidas, *((int*)arg));
+    fflush(stdout);
     if (H >= 2 && O >= 1) {
         sem_post(&H_sem);
         sem_post(&H_sem);
@@ -94,41 +106,56 @@ void* thread_H(void* arg) {
         O--;
     }
     else {
-        pthread_mutex_unlock(&mutex);
-    }    
+        if (moleculas_lidas == NUM_MOLECULAS) {
+            print_stock_and_finish_program();
+        } else {
+            pthread_mutex_unlock(&mutex);
+        }
+    }
     sem_wait(&H_sem);
     forma_h2o();
-    fflush(stdout);
+
     pthread_barrier_wait(&barreira);
 	return 0;
 }
 
+void print_stock_and_finish_program(){
+    printf("\nTodas as moléculas lidas!\n");
+    printf("Estoque H: %d\n", H);
+    printf("Estoque O: %d\n", O);
+    fflush(stdout);
+    destroi_variaveis();
+    exit(0);
+}
+
 void forma_h2o() {
-    // int sem_value_h;
-    // int sem_value_o;
-    // sem_getvalue(&H_sem, &sem_value_h);
-    // sem_getvalue(&O_sem, &sem_value_o);
-    // printf("\n---------------------------------------------\n\n");
-    printf("\nH20 Formada! (Threads atravessaram a barreira)\n");
-    // printf("Estoque H: %d\n", H);
-    // printf("Estoque O: %d\n", O);
-    // printf("Sem O Value: %d\n", sem_value_o);
-    // printf("Sem H Value: %d\n", sem_value_h);
-    printf("Total Moleculas Recebidas: %d\n", moleculas_lidas);
-    // printf("---------------------------------------------\n\n");
-    // if ((sem_value_h < 1 && sem_value_o < 1) && (moleculas_lidas == 100)) {
-    //     if(H > 0 || O > 0) {
-    //         printf("Impossível Produzir H2O (Deadlock).\n");
-    //     } else {
-    //         printf("Todas as Moleculas Possiveis Utilizadas!\n");
-    //     }
-    //     exit(0);
-    // }
+    int sem_value_h;
+    int sem_value_o;
+    sem_getvalue(&H_sem, &sem_value_h);
+    sem_getvalue(&O_sem, &sem_value_o);
+    pthread_mutex_lock(&print_mutex);
+    if(sem_value_h == 0 && sem_value_o == 0) {
+        pode_imprimir = TRUE;
+    } else {
+        pode_imprimir = FALSE;
+    }
+    
+    if(pode_imprimir == TRUE) {
+        printf("\n---------------------------------------------");
+        printf("\nH20 Formada! (atravessaram a barreira)\n");
+        printf("Estoque H: %d\n", H);
+        printf("Estoque O: %d\n", O);
+        printf("---------------------------------------------\n\n");
+        fflush(stdout);
+        if (moleculas_lidas == NUM_MOLECULAS) {
+            print_stock_and_finish_program();
+        }
+    }
+    pthread_mutex_unlock(&print_mutex);
 }
 
 int main(int argc, char** argv) {
     inicia_variaveis();
     inicia_threads();
-    destroi_variaveis();
     return 0;
 }
